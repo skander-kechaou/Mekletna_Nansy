6e354db069d1b80c9b95607ed9434bdc74fe8e50
youssefressaissi <ressaissi.youssef@esprit.tn>
2022-12-10 14:57:26 +0000

    pukk

--- Views/assets/vendor/bootstrap/js/bootstrap.bundle.js
+++ Views/assets/vendor/bootstrap/js/bootstrap.bundle.js
@@ -3849,7 +3849,7 @@
   const CLASS_NAME_DROPDOWN_CENTER = 'dropdown-center';
   const SELECTOR_DATA_TOGGLE$3 = '[data-bs-toggle="dropdown"]:not(.disabled):not(:disabled)';
   const SELECTOR_DATA_TOGGLE_SHOWN = `${SELECTOR_DATA_TOGGLE$3}.${CLASS_NAME_SHOW$6}`;
-  const SELECTOR_MENU = '.dropdown-menu';
+  const SELECTOR_menu = '.dropdown-menu';
   const SELECTOR_NAVBAR = '.navbar';
   const SELECTOR_NAVBAR_NAV = '.navbar-nav';
   const SELECTOR_VISIBLE_ITEMS = '.dropdown-menu .dropdown-item:not(.disabled):not(:disabled)';
@@ -3888,7 +3888,7 @@
       this._parent = this._element.parentNode; // dropdown wrapper
       // todo: v6 revert #37011 & change markup https://getbootstrap.com/docs/5.2/forms/input-group/
 
-      this._menu = SelectorEngine.next(this._element, SELECTOR_MENU)[0] || SelectorEngine.prev(this._element, SELECTOR_MENU)[0] || SelectorEngine.findOne(SELECTOR_MENU, this._parent);
+      this._menu = SelectorEngine.next(this._element, SELECTOR_menu)[0] || SelectorEngine.prev(this._element, SELECTOR_menu)[0] || SelectorEngine.findOne(SELECTOR_menu, this._parent);
       this._inNavbar = this._detectNavbar();
     } // Getters
 
@@ -4119,7 +4119,7 @@
       };
     }
 
-    _selectMenuItem({
+    _selectmenuItem({
       key,
       target
     }) {
@@ -4151,7 +4151,7 @@
       });
     }
 
-    static clearMenus(event) {
+    static clearmenus(event) {
       if (event.button === RIGHT_MOUSE_BUTTON || event.type === 'keyup' && event.key !== TAB_KEY$1) {
         return;
       }
@@ -4166,9 +4166,9 @@
         }
 
         const composedPath = event.composedPath();
-        const isMenuTarget = composedPath.includes(context._menu);
+        const ismenuTarget = composedPath.includes(context._menu);
 
-        if (composedPath.includes(context._element) || context._config.autoClose === 'inside' && !isMenuTarget || context._config.autoClose === 'outside' && isMenuTarget) {
+        if (composedPath.includes(context._element) || context._config.autoClose === 'inside' && !ismenuTarget || context._config.autoClose === 'outside' && ismenuTarget) {
           continue;
         } // Tab navigation through the dropdown menu or events from contained inputs shouldn't close the menu
 
@@ -4213,7 +4213,7 @@
         event.stopPropagation();
         instance.show();
 
-        instance._selectMenuItem(event);
+        instance._selectmenuItem(event);
 
         return;
       }
@@ -4233,9 +4233,9 @@
 
 
   EventHandler.on(document, EVENT_KEYDOWN_DATA_API, SELECTOR_DATA_TOGGLE$3, Dropdown.dataApiKeydownHandler);
-  EventHandler.on(document, EVENT_KEYDOWN_DATA_API, SELECTOR_MENU, Dropdown.dataApiKeydownHandler);
-  EventHandler.on(document, EVENT_CLICK_DATA_API$3, Dropdown.clearMenus);
-  EventHandler.on(document, EVENT_KEYUP_DATA_API, Dropdown.clearMenus);
+  EventHandler.on(document, EVENT_KEYDOWN_DATA_API, SELECTOR_menu, Dropdown.dataApiKeydownHandler);
+  EventHandler.on(document, EVENT_CLICK_DATA_API$3, Dropdown.clearmenus);
+  EventHandler.on(document, EVENT_KEYUP_DATA_API, Dropdown.clearmenus);
   EventHandler.on(document, EVENT_CLICK_DATA_API$3, SELECTOR_DATA_TOGGLE$3, function (event) {
     event.preventDefault();
     Dropdown.getOrCreateInstance(this).toggle();
@@ -6552,7 +6552,7 @@
   const CLASS_NAME_SHOW$1 = 'show';
   const CLASS_DROPDOWN = 'dropdown';
   const SELECTOR_DROPDOWN_TOGGLE = '.dropdown-toggle';
-  const SELECTOR_DROPDOWN_MENU = '.dropdown-menu';
+  const SELECTOR_DROPDOWN_menu = '.dropdown-menu';
   const NOT_SELECTOR_DROPDOWN_TOGGLE = ':not(.dropdown-toggle)';
   const SELECTOR_TAB_PANEL = '.list-group, .nav, [role="tablist"]';
   const SELECTOR_OUTER = '.nav-item, .list-group-item';
@@ -6763,7 +6763,7 @@
       };
 
       toggle(SELECTOR_DROPDOWN_TOGGLE, CLASS_NAME_ACTIVE);
-      toggle(SELECTOR_DROPDOWN_MENU, CLASS_NAME_SHOW$1);
+      toggle(SELECTOR_DROPDOWN_menu, CLASS_NAME_SHOW$1);
       outerElem.setAttribute('aria-expanded', open);
     }
 
