5f8d0557df21782d914ccf9fb3cb9804a26c8e18
ArijMah <arij.mahouechi@esprit.tn>
2022-12-11 16:44:36 +0100

    basket

--- Views/supprimerpanier.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-include "../../Core/paniercore.php";
-$paniercore=new paniercore();
-if (isset($_POST["id_Panier"])){
-	$paniercore->supprimerpanier($_POST["id_Panier"]);
-	header('Location: index.php');
-}
-
-?>
