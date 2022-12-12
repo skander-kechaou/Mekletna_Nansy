2c5d524f65d7d73a290151cf0e4f7b09875948d3
youssefressaissi <ressaissi.youssef@esprit.tn>
2022-12-12 19:22:33 +0000

    ff

--- Views/addRegion.php
+++ Views/addRegion.php
@@ -98,7 +98,7 @@ if ((
                                 <a class="dropdown-item" href="listOrder.php">orders Report</a>
                                 <a class="dropdown-item" href="addRegion.php">Region managment</a>
                                 <a class="dropdown-item" href="listBasket.php">Basket List</a>
-                                <a class="dropdown-item" href="listRegions.php">List Regions</a>
+                                <a class="dropdown-item" href="region management.php">List Regions</a>
                                 
                             </div>
                         </li>
@@ -113,7 +113,7 @@ if ((
                                 <a class="dropdown-item" href="listfooditem2.php">food items</a>
                                 <a class="dropdown-item" href="listmenu2.php">Menus</a>
                                 <a class="dropdown-item" href="addfooditem.php">Food item managment</a>
-                                <a class="dropdown-item" href="addMenu.php">Menu managment</a>
+                                
                                 </div>
                             </a>
                         </li>
--- Views/listClients.php
+++ Views/listClients.php
@@ -66,7 +66,7 @@ $list = $clientC->listClients();
                                 <a class="dropdown-item" href="listOrder.php">orders Report</a>
                                 <a class="dropdown-item" href="addRegion.php">Region managment</a>
                                 <a class="dropdown-item" href="listBasket.php">Basket List</a>
-                                <a class="dropdown-item" href="listRegions.php">List Regions</a>
+                                <a class="dropdown-item" href="region management.php">List Regions</a>
                                 
                             </div>
                         </li>
@@ -81,7 +81,7 @@ $list = $clientC->listClients();
                                 <a class="dropdown-item" href="listfooditem2.php">food items</a>
                                 <a class="dropdown-item" href="listmenu2.php">Menus</a>
                                 <a class="dropdown-item" href="addfooditem.php">Food item managment</a>
-                                <a class="dropdown-item" href="addMenu.php">Menu managment</a>
+                               
                                 </div>
                             </a>
                         </li>
