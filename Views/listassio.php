e26924e934673405c23428f47230b0587584ee76
skander-kechaou <skander.kechaou@esprit.tn>
2022-12-13 23:16:35 +0100

    new

--- Views/listassio.php
+++ Views/listassio.php
@@ -137,7 +137,10 @@ $result = mysqli_query($conn,$query);
             </div>
             <div class="row mt-2">
             <div class="col-12">
+                <div class="card">
+                    <div class="card-header">Association Statistics</div>
              <div id="piechart" style="width: 1000px; height: 500px;"></div>
+                 </div>
             </div>
             </div>
 
